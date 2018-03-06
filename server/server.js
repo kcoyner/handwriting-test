e9831624d5b2dcd82a75a6ca33360011bfb05b8b
supertopoz <deepsleepsounds@gmail.com>
2018-03-06 19:06:47 +0900

    first commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const Bundler = require('parcel-bundler');
+const Path = require('path');
+const app = require('express')();
+
+const file = Path.join(__dirname, '../index.html');
+
+// Bundler options
+const options = {
+ // outDir: './dist', // The out directory to put the build files in, defaults to dist
+  outFile: 'index.html', // The name of the outputFile
+//  publicUrl: './', // The url to server on, defaults to dist
+  watch: true, // whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+  cache: true, // Enabled or disables caching, defaults to true
+  cacheDir: '.cache', // The directory cache gets put in, defaults to .cache
+  minify: true, // Minify files, enabled if process.env.NODE_ENV === 'production'
+  target: 'browser', // browser/node/electron, defaults to browser
+  https: false, // Server files over https or http, defaults to false
+  logLevel: 3, // 3 = log everything, 2 = log warnings & errors, 1 = log errors
+  hmrPort: 0, // The port the hmr socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
+  sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (not supported in minified builds yet)
+  hmrHostname: '', // A hostname for hot module reload, default to ''
+  detailedReport: false // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
+};
+
+
+// Initialise a new bundler using a file and options (for options and file see the bundler documentation)
+const bundler = new Bundler(file, options);
+
+// Let express use the bundler middleware, this will let parcel handle every request over your express server
+app.use(bundler.middleware());
+
+// Listen on port 8080
+console.log('Running on port: 8080')
+app.listen(8080);
